a247cb2fa6dc90e1888b406f1ccc2f65c22313cb
phatgg221 <129394719+phatgg221@users.noreply.github.com>
2025-05-14 15:09:10 +0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,25 +1,14 @@
-
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI
 from tensorflow.keras.models import load_model
-from PIL import Image
-import numpy as np
-import io
 
 app = FastAPI()
 
 try:
     loaded_best_model = load_model("best_vgg_model.h5")
-    print(f"Model loaded successfully from {"best_vgg_model.h5"}")
+    print(f"Model loaded successfully from 'best_vgg_model.h5'")
 except Exception as e:
     print(f"Error loading model: {e}")
     exit()
-
-
-# def preprocess_image(image_bytes):
-#     image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
-#     image = image.resize((128, 128))  # Change size if needed
-#     img_array = np.array(image) / 255.0
-#     return np.expand_dims(img_array, axis=0)
 
 @app.get("/get")
 def read_root():
@@ -27,8 +16,3 @@ def read_root():
         return {"Hello": "World"}
     else:
         return {"nothing to show"}
-    
-
-# @app.get("/predict")
-# async def predict():
-#     return {"prediction"}
